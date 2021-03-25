11a3e8ade9febe30d012bf62f8d90d6f99d0cf85
Darius <62259897+DariusDaskevicius@users.noreply.github.com>
2021-03-25 03:49:19 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+from enum import Enum
+import os
 
 players = ['X', 'O']
+unavailable_fields = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 row_names = {
     'A': 0,
     'B': 1,
@@ -45,45 +48,69 @@ def render_field():
         print('\n--------------------------------------------------------')
 
 def field_winner(player):
+    count = 0
+    check_x = []
     for field in game_field:
-        if field[0][0] == field[1][1] and field[1][1] == field[2][2] and field[0][0] != '#': #check X
-            return True
-        if field[2][0] == field[1][1] and field[1][1] == field[0][2] and field[1][1] != '#': #check X
-            return True
+        row = []
+        for i in range(3):
+            row.append(field[i])
+            for y in row:
+                check_x.append(y)
 
-        for i in range(3): #check rows
-            if ((field[i][0] == field[i][1]) and (field[i][1] == field[i][2]) and (field[i][0] != '#')):
-                return True
+        for i in row:
+            if len(set(i)) == 1:
+                unavailable_fields.append(count)
 
-            check_col = [[], [], []]
-            for row in field: # check columns
-                check_col[i].append(str(row[i]))
-            if (check_col[i][0] == check_col[i][1]) and (check_col[i][1] == check_col[i][2]) and (check_col[i][0] != '#'):
-                return True
+        collumns = list(zip(*row))
+        for i in collumns:
+            if len(set(i)) == 1:
+                unavailable_fields.append(count)
 
+        if list(set(check_x[::4])) == 1:
+            unavailable_fields.append(count)
+
+        if list(set(check_x[-3::-2])[:-1]) == 1:
+            unavailable_fields.append(count)
+
+        count += 1
+
+def game_winner()
+    pass
+
+def choose_your_field():
+    render_field()
+    current_field = int(input())
+    current_field -= 1
+    return current_field
 
 def your_turn():
-    current_field = 0
+    current_field = choose_your_field()
     game_ON = True
+    os.system('clear')
     while game_ON:
         for player in players:
-            if field_winner(player):
-                game_ON = False
-
             render_field()
             turn = input()
             row_name, column = turn
             row_index = row_names[row_name]
             column_index = int(column) - 1
-            game_field[current_field][row_index][column_index] = player
+            if current_field in unavailable_fields:
+                print('Please choose another field!')
+                choose_another_field = True
+                while choose_another_field:
+                    current_field = choose_your_field()
+                    if current_field not in unavailable_fields:
+                        choose_another_field = False
+            else:
+                game_field[current_field][row_index][column_index] = player
 
-            if (row_name == 'A'):
-                current_field = column_index
-            if (row_name == 'B'):
-                current_field = column_index + 3
-            if (row_name == 'C'):
-                current_field = column_index + 6
+                if (row_name == 'A'):
+                    current_field = column_index
+                if (row_name == 'B'):
+                    current_field = column_index + 3
+                if (row_name == 'C'):
+                    current_field = column_index + 6
 
-
-
+            field_winner(player)
+            os.system('clear')
 your_turn()
