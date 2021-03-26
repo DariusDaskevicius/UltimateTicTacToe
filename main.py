cbc4f641a5cb52655a50579db30a83ffe494c223
Darius <62259897+DariusDaskevicius@users.noreply.github.com>
2021-03-26 04:03:40 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,54 +1,55 @@
 import os
 
-class Hasher:
-    def __init__(self, row):
-        self.row = row
+players = ['X', 'O']
+ROWS = COLS = 3
+field_to_play = []
 
-    def __hash__(self):
-        return hash(''.join(self.row))
+for i in range(ROWS * COLS):
+    one_field = [['1#', '4#', '7#'],
+                 ['2#', '5#', '8#'],
+                 ['3#', '6#', '9#']]
+    field_to_play.append(one_field)
 
-    def __eq__(self, other):
-        return hash(self) == hash(other)
+game_winner = ['1#', '4#', '7#', '2#', '5#', '8#', '3#', '6#', '9#']
+
+
+# class Hasher:
+#     def __init__(self, row):
+#         self.row = row
+#
+#     def __hash__(self):
+#         return hash(''.join(self.row))
+#
+#     def __eq__(self, other):
+#         return hash(self) == hash(other)
 
 class Board:
     def __init__(self):
         self.field = []
-        self.ROWS = self.COLS = 3
-
-    def create_game_field(self):
-        for i in range(self.ROWS * self.COLS):
-            one_field = [['#', '#', '#'],
-                         ['#', '#', '#'],
-                         ['#', '#', '#']]
-            self.field.append(one_field)
-        return self.field
-
-class Render:
-    def __init__(self):
         self.first_f = 0
         self.last_f = 3
         self.field_count = 0
         self.field_markup = 'ABC'
-        # self.board = Board().create_game_field()
 
-    def render_board(self, board):
+    def render_board(self):
+        global field_to_play
         print('--1----2----3-------1----2--- 3-------1----2----3----')
         for i in range(0, 3):
-            for field in board[self.first_f:self.last_f]:
+            for field in field_to_play[self.first_f:self.last_f]:
                 if field[0]:
                     print(f'{field[0]} |', end=' ')
             print(self.field_markup[self.field_count], end=' ')
             print('\n')
             self.field_count += 1
 
-            for field in board[self.first_f:self.last_f]:
+            for field in field_to_play[self.first_f:self.last_f]:
                 if field[1]:
                     print(f'{field[1]} |', end=' ')
             print(self.field_markup[self.field_count], end=' ')
             print('\n')
             self.field_count += 1
 
-            for field in board[self.first_f:self.last_f]:
+            for field in field_to_play[self.first_f:self.last_f]:
                 if field[2]:
                     print(f'{field[2]} |', end=' ')
             print(self.field_markup[self.field_count], end=' ')
@@ -60,8 +61,7 @@ class Render:
 
 class Game:
     def __init__(self):
-        self.board = Board().create_game_field()
-        self.players = ['X', 'O']
+        self.game_field = Board
         self.unavailable_fields = []
         self.winned_fields = [['#', '#', '#'],
                               ['#', '#', '#'],
@@ -71,20 +71,21 @@ class Game:
             'B': 1,
             'C': 2
         }
-        self.game_field = Render()
 
-    def game_winner(self, player):
-        board = self.board
-        game_field = self.game_field.render_board(board)
+    def field_winner(self, player):
+        global field_to_play
+        global game_winner
         unavailable_fields = self.unavailable_fields
         count = 0
-        check_x = []
-        for field in game_field:
+        for field in field_to_play:
+            check_x = []
             row = []
             for i in range(3):
                 row.append(field[i])
-                for y in row:
-                    check_x.append(y)
+
+            for single_row in row:
+                for element in single_row:
+                    check_x.append(element)
 
             for i in row:
                 if len(set(i)) == 1:
@@ -95,51 +96,56 @@ class Game:
                 if len(set(i)) == 1:
                     unavailable_fields.append(count)
 
-            if list(set(check_x[::4])) == 1:
+            if list(set(check_x[0::4])) == 1:
                 unavailable_fields.append(count)
 
-            if list(set(check_x[-3::-2])[:-1]) == 1:
+            if list(set(check_x[-3::-2][:-1])) == 1:
                 unavailable_fields.append(count)
 
             count += 1
 
     def choose_your_field(self):
+        game_field = self.game_field()
+        game_field.render_board()
         print('Choose field!')
         current_field = int(input())
         current_field -= 1
         return current_field
 
     def play(self):
-        players = self.players
+        global field_to_play
+        global players
         unavailable_fields = self.unavailable_fields
-        game_field = self.game_field
-        current_field = self.choose_your_field()
+        current_field = self.choose_your_field
         row_names = self.row_names
         game_ON = True
+        field_number = int(current_field())
         os.system('clear')
         while game_ON:
             for player in players:
-                game_field
+                game_field = self.game_field()
+                game_field.render_board()
                 turn = input()
                 row_name, column = turn
                 row_index = row_names[row_name]
                 column_index = int(column) - 1
-                if current_field in unavailable_fields:
+                if field_number in unavailable_fields:
                     print('Please choose another field!')
                     choose_another_field = True
                     while choose_another_field:
-                        current_field = self.choose_your_field()
-                        if current_field not in unavailable_fields:
+                        current_field()
+                        field_number = current_field()
+                        if field_number not in unavailable_fields:
                             choose_another_field = False
                 else:
-                    game_field[current_field][row_index][column_index] = player
+                    field_to_play[field_number][row_index][column_index] = player
 
                     if (row_name == 'A'):
-                        current_field = column_index
+                        field_number = column_index
                     if (row_name == 'B'):
-                        current_field = column_index + 3
+                        field_number = column_index + 3
                     if (row_name == 'C'):
-                        current_field = column_index + 6
+                        field_number = column_index + 6
 
                 self.game_winner(player)
                 os.system('clear')
