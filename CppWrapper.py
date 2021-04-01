36abea434bfd98a527074c3638d609c5f8b81475
Darius <62259897+DariusDaskevicius@users.noreply.github.com>
2021-04-01 08:16:53 -0400

    Add files via upload

--- CppWrapper.py
+++ CppWrapper.py
@@ -1,3 +1,4 @@
+
 class CppWrapper:
     c_types = ("UI16", "UI8", "SI16", "SI32")
     c_elements = ("namespace", "class", "enum")
@@ -6,6 +7,7 @@ class CppWrapper:
         self.file = file
 
     def elem(self):
+        parents = {}
         elements = []
         FileHandler = open(f"{self.file}", "r")
         for line in FileHandler:
@@ -15,43 +17,56 @@ class CppWrapper:
                 if elem not in stripped_line:
                     continue
 
-                stripped_line = stripped_line.replace(elem, ('-'*i + '->'))  # why replace do not work here???
+                stripped_line = stripped_line.replace(elem, ('-'*i + '->'))
                 elements.append(stripped_line)
 
-            # if 'namespace' in stripped_line:
-            #     stripped_line = stripped_line.replace('namespace', '->')  # why replace do not work here???
-            #     elements.append(stripped_line)
-            # if 'class' in stripped_line:
-            #     stripped_line = stripped_line.replace('class', '-->')  # why replace do not work here???
-            #     elements.append(stripped_line)
-            # if 'enum' in stripped_line:
-            #     stripped_line = stripped_line.replace('enum', '--->')  # why replace do not work here???
-            #     elements.append(stripped_line)
+
 
             if any(t in stripped_line for t in self.c_types):
-            # if 'UI16' in stripped_line or 'UI8' in stripped_line or 'UI8' in stripped_line or 'SI16' in stripped_line:
                 stripped_line = stripped_line[:stripped_line.index(';')]
                 stripped_line = '--->' + stripped_line[stripped_line.index(' '):]
                 elements.append(stripped_line)
 
         return elements
 
+    def get_el(self):
+        dict = {}
+        keys = {}
+        index = 0
+        keys[index] = 'root'
+        dict[keys[index]] = []
+        index += 1
+
+        FileHandler = open(f"{self.file}", "r")
+        for line in FileHandler:
+            stripped_line = line.strip()
+            print('0')
+            if '{' in stripped_line:
+                print('2')
+                index += 1
+
+            elif '}' in stripped_line:
+                print('3')
+                index -= 1
+
+            else: #elem in stripped_line:
+                for i, elem in enumerate(self.c_elements):
+                    if elem in stripped_line:
+                        keyword = stripped_line.replace(elem, '')
+                        dict[(keys[index-1])].append(keyword)
+                        keys[index] = keyword
+                        dict[keyword] = []
+                        # print(dict[(keys[index-1])])
+
+        return dict
 
 cw = CppWrapper('dummy')
-elements = cw.elem()
+elements = cw.get_el()
 
-for i in elements:
-    print(i)
+for key, value in elements.items():
+    print(f'{key} : {value}')
 
-for element in elements["FlowMonitor"]:
-    print(element)
-
-    """
-    ---> FlowMonitoringStates, lineno = 5
-    ---> floatSwitchTimeoutCounter_, lineno = 6
-    ---> balancingChamberStateMemory1_, lineno = 7
-    ---> balancingChamberStateMemory2_, lineno = 8
-    """
+# print(elements)
 
 """
 cleaning
